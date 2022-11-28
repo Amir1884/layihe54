848c114206185b7cdef09d4cada7692380cafccf
Amir1884 <114072557+Amir1884@users.noreply.github.com>
2022-11-28 23:25:50 +0400

    Add files via upload

---
+++ Layihe-M3/assets/main.js/emir.js
@@ -0,0 +1,24 @@
+// const rubl1 = document.getElementById('rubl1');
+// const dollar1 = document.getElementById('dollar1');
+// const euro1 = document.getElementById('euro1');
+// const gbp1 = document.getElementById('gbp1');
+// const rubl2 = document.getElementById('rubl2');
+// const dollar2 = document.getElementById('dollar2');
+// const euro2 = document.getElementById('euro2');
+// const gbp2 = document.getElementById('gbp2');
+// let buton = document.querySelector('.buton');
+// const divinput1 = document.getElementsByName('input1')[0];
+// const divinput2 = document.getElementsByName('input2')[0];
+// let table1 = document.querySelector('.table1');
+// let table2 = document.querySelector('.table2');
+
+// function convertor(a) {
+// if(a.target.className== "currency" ||)
+// }
+
+let num = prompt('iki reqemli eded daxil edin');
+while ((num < 100) && (num > 10)) {
+    num = +num + 7;
+    console.log(num);
+}
+console.log(the cycle is over);
